f000c5e007e73f07ef0e03b7a405450614d33770
[JesusEscano] <[jesusescano83@iesamachado.org]>
2023-01-15 16:45:10 +0100

    mas aun

--- javascript/circulos.js
+++ javascript/circulos.js
@@ -1,4 +1,11 @@
 function redondo(params) {
-    document.getElementById(params).style.backgroundColor="white"
+    document.getElementById(params).style.backgroundColor="transparent";
+
 }
 
+
+function recupera(params) {
+    document.getElementById("b1").style.backgroundColor="red";
+    document.getElementById("b2").style.backgroundColor="green";
+    document.getElementById("b3").style.backgroundColor="blue";
+}
