71ea461797644ec18252f99de7699f68ce5dbdec
[JesusEscano] <[jesusescano83@iesamachado.org]>
2023-03-18 15:30:25 +0100

    guarda

--- javascript/carrito/js/script.js
+++ javascript/carrito/js/script.js
@@ -1,23 +1,23 @@
 
-var portatil=0;
-function sumaport() {
-    portatil+=1
-    document.getElementById("cpor").innerHTML = "En carrito = "+portatil;
+var monitor=0;
+function sumamon() {
+    monitor+=1
+    document.getElementById("cmon").innerHTML = "En carrito = "+monitor;
 }
-function restaport() {
-    if (portatil<=1){document.getElementById("cpor").innerHTML = ""}
-    else {portatil-=1
-    document.getElementById("cpor").innerHTML = "En carrito = "+portatil};
+function restamon() {
+    if (monitor<=1){document.getElementById("cmon").innerHTML = ""}
+    else {monitor-=1
+    document.getElementById("cmon").innerHTML = "En carrito = "+monitor};
 }
-var hdd=0;
-function sumahdd() {
-    hdd+=1
-    document.getElementById("chdd").innerHTML = "En carrito = "+hdd;
+var router=0;
+function sumarou() {
+    router+=1
+    document.getElementById("crou").innerHTML = "En carrito = "+router;
 }
-function restahdd() {
-    if (hdd<=1){document.getElementById("chdd").innerHTML = ""}
-    else {hdd-=1
-    document.getElementById("chdd").innerHTML = "En carrito = "+hdd};
+function restarou() {
+    if (router<=1){document.getElementById("crou").innerHTML = ""}
+    else {router-=1
+    document.getElementById("crou").innerHTML = "En carrito = "+router};
 }
 var key=0;
 function sumakey() {
@@ -39,11 +39,11 @@ function restamou() {
     else {mou-=1
     document.getElementById("cmou").innerHTML = "En carrito = "+mou};
 }
-function carport() {
-document.getElementById("carp").innerHTML = "<p>Procesador: Intel® Core™ i3-1115G4 (2 núcleos, frecuencia turbo máxima de 4.10 GHz, 6 MB Intel® Smart Cache)</p><p>Memoria RAM: 8 GB DDR4 3200 MHz (4GB integradas + 4GB SO-DIMM); Ampliable hasta 12 GB (1x SO-DIMM)</p><p>Almacenamiento: 512 GB SSD M.2 2242 PCIe 3.0x4 NVMe</p><p>Unidad óptica: No</p><p>Display: 15.6' FHD (1920x1080) TN, 250 nits, Antirreflectante, 45% NTSC</p><p>Controlador gráfico Integrado: Intel UHD Graphics</p><p>Conectividad:</p><li>Ethernet 100/1000M</li><li>WiFi 802.11ac (2x2) + Bluetooth 5.0</li><p>Webcam: HD 720p con tapa de privacidad</p><p>Micrófono: Sí</p><p>Audio 2 altavoces estéreo de 1.5 W, Dolby Audio</p><p>Batería Integrada: 38 Wh</p><p>Conexiones:</p><li>1 x USB 3.2 Gen 1</li><li>1 x USB-C 3.2 Gen 1 (solo admite transferencia de datos)</li><li>1 x USB 2.0</li><li>1 x HDMI 1.4b</li><li>1 x Combo auriculares/micrófono jack (3.5 mm)</li><li>1 x Ethernet (RJ-45)</li><p>Firmware: TPM 2.0</p><p>Ranura de seguridad: Kensington Nano</p><p>Sistema operativo: Windows 10 Home 64 bits</p><p>Dimensiones (Ancho x Profundidad x Altura): 359,2 x 235,8 x 19,9 mm</p><p>Peso: 1,7 kg</p><p>Color: Negro</p></td>";
+function carmon() {
+document.getElementById("cmon").innerHTML = "<p>Procesador: Intel® Core™ i3-1115G4 (2 núcleos, frecuencia turbo máxima de 4.10 GHz, 6 MB Intel® Smart Cache)</p><p>Memoria RAM: 8 GB DDR4 3200 MHz (4GB integradas + 4GB SO-DIMM); Ampliable hasta 12 GB (1x SO-DIMM)</p><p>Almacenamiento: 512 GB SSD M.2 2242 PCIe 3.0x4 NVMe</p><p>Unidad óptica: No</p><p>Display: 15.6' FHD (1920x1080) TN, 250 nits, Antirreflectante, 45% NTSC</p><p>Controlador gráfico Integrado: Intel UHD Graphics</p><p>Conectividad:</p><li>Ethernet 100/1000M</li><li>WiFi 802.11ac (2x2) + Bluetooth 5.0</li><p>Webcam: HD 720p con tapa de privacidad</p><p>Micrófono: Sí</p><p>Audio 2 altavoces estéreo de 1.5 W, Dolby Audio</p><p>Batería Integrada: 38 Wh</p><p>Conexiones:</p><li>1 x USB 3.2 Gen 1</li><li>1 x USB-C 3.2 Gen 1 (solo admite transferencia de datos)</li><li>1 x USB 2.0</li><li>1 x HDMI 1.4b</li><li>1 x Combo auriculares/micrófono jack (3.5 mm)</li><li>1 x Ethernet (RJ-45)</li><p>Firmware: TPM 2.0</p><p>Ranura de seguridad: Kensington Nano</p><p>Sistema operativo: Windows 10 Home 64 bits</p><p>Dimensiones (Ancho x Profundidad x Altura): 359,2 x 235,8 x 19,9 mm</p><p>Peso: 1,7 kg</p><p>Color: Negro</p></td>";
 }
-function carhdd() {
-document.getElementById("hdd").innerHTML =   "<p>Capacidad: 2 TB</p><p>Serie: Toshiba Canvio Basics</p><p>Interfaz: SATA / USB 3.0</p><p>Tasa de transferencia: hasta 5 Gbit/s</p><p>Factor de forma: 2,5 pulgadas</p><p>Equipamiento: indicador LED de encendido, pies de goma</p><p>Alimentación: puerto USB</p><p>Sistema de archivos: NTFS</p><p>Medidas y peso: 78 mm*109 mm*14 mm / 149 g</p><p>Color negro</p>";
+function carou() {
+document.getElementById("rou").innerHTML =   "<p>Capacidad: 2 TB</p><p>Serie: Toshiba Canvio Basics</p><p>Interfaz: SATA / USB 3.0</p><p>Tasa de transferencia: hasta 5 Gbit/s</p><p>Factor de forma: 2,5 pulgadas</p><p>Equipamiento: indicador LED de encendido, pies de goma</p><p>Alimentación: puerto USB</p><p>Sistema de archivos: NTFS</p><p>Medidas y peso: 78 mm*109 mm*14 mm / 149 g</p><p>Color negro</p>";
 }
 function carkey() {
 document.getElementById("keyb").innerHTML = "<p>Antifantasma: Si</p><p>Longitud de cable: 1,5 m</p><p>Alimentación: USB</p><p>Color del producto: Negro Monocromo</p><p>Material: Acrilonitrilo butadieno estireno (ABS)</p><p>Tipo de retroiluminación: LED RGB</p><p>Dimensiones de teclado (Ancho x Profundidad x Altura): 292 x 102 x 37 mm</p><p>Peso del teclado: 400 g</p><p>Formato del teclado: Mini</p><p>Idioma del teclado: Español</p><p>Interruptor del teclado: Interruptor mecánico</p><p>Teclado numérico: No</p><p>Tecnología de switch del teclado: Outemu Red</p>";
